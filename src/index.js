ee9ba713b5e5b36c8e45fd2fd67fff23978dc1b5
Preslav977 <p.cvetanow@gmail.com>
2023-07-14 09:50:39 +0300

    Created a function that process the JSON data from API which returns a objects

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 function getLocationAPI() {
   fetch(
-    "https://api.weatherapi.com/v1/current.json?key=e5de975d30ff49eeb9a70436231107&q=trastenik",
+    "https://api.weatherapi.com/v1/current.json?key=e5de975d30ff49eeb9a70436231107&q=pleven",
     { mode: "cors" }
   )
     .then((response) => response.json())
-    .then((response) => {
-      console.log(response.location);
-    });
+    .then(processLocationAPI);
 }
 
 getLocationAPI();
+
+function processLocationAPI(weatherData) {
+  const weatherLocationData = weatherData.location;
+  console.log(weatherLocationData);
+  const currentWeatherLocationData = weatherData.current;
+  console.log(currentWeatherLocationData);
+}
