121f0deeb07ccb1c2d1b6b5373cda2df2a357158
Preslav977 <p.cvetanow@gmail.com>
2023-07-14 08:56:15 +0300

    Created a function that fetch the API and returns weather data for the location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Testing webpack");
+function getLocationAPI() {
+  fetch(
+    "https://api.weatherapi.com/v1/current.json?key=e5de975d30ff49eeb9a70436231107&q=trastenik",
+    { mode: "cors" }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response.location);
+    });
+}
+
+getLocationAPI();
