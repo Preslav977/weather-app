2aba5b2807594b61bb41d20120cf679f79e72556
Preslav977 <p.cvetanow@gmail.com>
2023-07-17 16:09:38 +0300

    Created function that process the API location data

--- src/index.js
+++ src/index.js
@@ -12,10 +12,33 @@ async function fetchUserLocation() {
     console.log(error);
   }
 }
+
+let currentWeatherObj = {};
+
+function processCurrentWeatherJSON(weatherData) {
+  currentWeatherObj = {
+    condition: weatherData.current.condition.text,
+    city: weatherData.location.name,
+    time: weatherData.location.localtime,
+    degreesCelsius: weatherData.current.temp_c,
+    degreesFahrenheit: weatherData.current.temp_f,
+    todayWeatherIcon: weatherData.current.condition.icon,
+    feelsLikeC: weatherData.current.feelslike_c,
+    feelsLikeF: weatherData.current.feelslike_f,
+    humidity: weatherData.current.humidity,
+    chanceOfRain: weatherData.forecast.forecastday[0].day.daily_chance_of_rain,
+    windSpeed: weatherData.current.wind_kph,
+    windSpeedMph: weatherData.current.wind_mph,
+  };
+  console.log(currentWeatherObj);
+  return currentWeatherObj;
+}
+
 userForm.addEventListener("submit", async (e) => {
   const weatherData = await fetchUserLocation();
   console.log(weatherData);
   e.preventDefault();
   fetchUserLocation();
+  processCurrentWeatherJSON(weatherData);
   userForm.reset();
 });
